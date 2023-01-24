68210c4f2cbc3c85d14ac5487f0b43ddc24f3385
MonicaDeBona <debona.monica@outlook.it>
2023-01-24 16:00:09 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Badwords</title>
+</head>
+
+<body>
+    <main>
+        <h1>
+            Ciao
+        </h1>
+        <h1>
+            <?php
+            echo 'Ciao';
+            ?>
+        </h1>
+    </main>
+</body>
+
+</html>
