caceacc54be6bf579a6393ca181587a977966102
MonicaDeBona <debona.monica@outlook.it>
2023-01-24 18:00:36 +0100

    Add html in censor and style

--- censor.php
+++ censor.php
@@ -1,27 +1,54 @@
-<p>
-    Your text:
-    <?php
-    $userParagraph = $_POST['paragraph'];
-    echo $userParagraph;
-    ?>
-</p>
-<p>
-    Paragraph length:
-    <?php
-    echo strlen($userParagraph);
-    ?>
-</p>
-<p>
-    Your text with censored word:
-    <?php
-    $censoredWord = $_POST['censor'];
-    $newUserParagraph = str_replace(strtolower($censoredWord), '***', strtolower($userParagraph));
-    echo $newUserParagraph;
-    ?>
-</p>
-<p>
-    Paragraph length:
-    <?php
-    echo strlen($newUserParagraph);
-    ?>
-</p>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+
+    <!-- Google font-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;700;800&display=swap" rel="stylesheet">
+
+    <title></title>
+</head>
+
+<body>
+    <div class="container">
+        <main>
+            <div class="response">
+                <p>
+                    Your text:
+                    <?php
+                    $userParagraph = $_POST['paragraph'];
+                    echo $userParagraph;
+                    ?>
+                </p>
+                <p>
+                    Paragraph length:
+                    <?php
+                    echo strlen($userParagraph);
+                    ?>
+                </p>
+                <p>
+                    Your text with censored word:
+                    <?php
+                    $censoredWord = $_POST['censor'];
+                    $newUserParagraph = str_replace(strtolower($censoredWord), '***', strtolower($userParagraph));
+                    echo $newUserParagraph;
+                    ?>
+                </p>
+                <p>
+                    Paragraph length:
+                    <?php
+                    echo strlen($newUserParagraph);
+                    ?>
+                </p>
+            </div>
+        </main>
+    </div>
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -5,21 +5,28 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+
+    <!-- Google font-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;700;800&display=swap" rel="stylesheet">
+
     <title>PHP Badwords</title>
 </head>
 
 <body>
-    <header>
-        <h1>Badwords</h1>
-    </header>
     <main>
-        <form action="./censor.php" method="POST">
-            <label for="paragraph">Type text</label>
-            <input type="text" id="paragraph" name="paragraph">
-            <label for="censor">Type the word to censor</label>
-            <input type="text" id="censor" name="censor">
-            <button type="submit">Send</button>
-        </form>
+        <div class="my-form">
+            <h2>Badwords</h2>
+            <form action="./censor.php" method="POST">
+                <label for="paragraph">Type text</label>
+                <input type="text" id="paragraph" name="paragraph" placeholder="Text">
+                <label for="censor">Type the word to censor</label>
+                <input type="text" id="censor" name="censor" placeholder="Word">
+                <button type="submit">Send</button>
+            </form>
+        </div>
     </main>
 </body>
 
